869d04abda05f5132100f1a13094c128b1b292c1
Alicia <chan.aliciaw@gmail.com>
2020-04-29 02:43:53 -0400

    updated pages

---
+++ assets/data/pvd.js
@@ -0,0 +1,49 @@
+var array = [
+    {
+        "name": "Amber Czesnolowicz", 
+        "note": ""
+
+    },
+    {
+        "name": "Anna France",  
+        "note": "" 
+    },
+    {
+        "name": "Charice Lim", 
+        "note": "" 
+    },
+    {
+        "name": "", 
+        "note": "" 
+    },
+    {
+        "name": "", 
+        "note": "" 
+    },
+    {
+        "name": "", 
+        "note": "" 
+    },
+    {
+        "name": "", 
+        "note": "" 
+    },
+    {
+        "name": "", 
+        "note": "" 
+    },
+    {
+        "name": "", 
+        "note": "" 
+    },
+    {
+        "name": "", 
+        "note": "" 
+    },
+
+
+]
+
+
+
+module.exports = array;
